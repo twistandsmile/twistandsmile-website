f63abd65942e33ff2710d198a592875d857edf9b
Clément Roure <croure@yupiik.com>
2025-11-25 17:43:44 +0200

    DONE - home page + articles page

---
+++ js/blog-articles.js
@@ -0,0 +1,26 @@
+const articles = [
+    {
+        "title": "My next big motorcycle trip",
+        "date": "2025-06-25",
+        "description": "I'm foreseeing some great time coming soon. Find out where I'll go !",
+        "duration": "6 min",
+        "img": "preview.jpeg",
+        "slug": "my-next-big-motorcycle-trip"
+    },
+    {
+        "title": "6 reasons why you don't need a big powerful motorcycle",
+        "date": "2025-05-19",
+        "description": "Find out about the advantages of going small, that convinced me that I was done with big motorbikes",
+        "duration": "17 min",
+        "img": "himalayan.jpg",
+        "slug": "6-reasons-why-you-dont-need-a-big"
+    },
+    {
+        "title": "I defied the trend, and went back to a \"beginner\" motorcycle",
+        "date": "2025-05-10",
+        "description": "Discover how by going less, I actually got more !",
+        "duration": "5 min",
+        "img": "preview.jpg",
+        "slug": "i-defied-the-trend-and-went-back"
+    }
+];
