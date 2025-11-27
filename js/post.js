f5ed9f44ac80f29b6f8a519db2a155839fa9aec1
Clement Roure <roure.clement@gmail.com>
2025-11-28 00:03:31 +0200

    display comments

--- js/post.js
+++ js/post.js
@@ -9,7 +9,7 @@ $(function() {
     $('#title-area > h4').text(article.description);
     $('#title-area > small').text(new Date(article.date).toDateString() + ' · ' + article.duration + ' read');
 
-    $('.comment-form-area').load('/html/includes/comment-form.html', function() {
+    $('.comment-area').load('/html/includes/comment-form.html', function() {
         $('input[name="slug"]').val(slug);
         document.getElementById("comment-form").addEventListener("submit", async (e) => {
             e.preventDefault();
@@ -30,6 +30,45 @@ $(function() {
             }
         });
 
+        $('.comment-area').append('<div class="comments-wrapper"><div class="comments-container"></div></div>');
+        if(comments.comments.length > 0) {
+            for(comment of comments.comments) {
+                $('.comments-container').append('<div class="comment"><div class="comment-header"><span class="comment-name">' + comment.name + '</span><span class="comment-date">' + comment.date + '</span></div><span class="comment-text">' + comment.comment + '</span></div>');
+            }
+            $(".comment-date").each(function() {
+                const iso = $(this).text().trim();
+                const date = new Date(iso);
+                if (isNaN(date)) return;
+
+                // put full date in tooltip
+                $(this).attr("title", date.toDateString());
+
+                const now = new Date();
+                const seconds = Math.floor((now - date) / 1000);
+                let display;
+                const intervals = {
+                    year:   365 * 24 * 60 * 60,
+                    month:  30 * 24 * 60 * 60,
+                    day:    24 * 60 * 60,
+                    hour:   60 * 60,
+                    minute: 60,
+                    second: 1
+                };
+                for (const unit in intervals) {
+                    const value = Math.floor(seconds / intervals[unit]);
+                    if (value >= 1) {
+                        display = value + " " + unit + (value > 1 ? "s" : "") + " ago";
+                        break;
+                    }
+                }
+                if (!display) {
+                    display = "just now";
+                }
+                $(this).text(display);
+            });
+        } else {
+            $('.comments-container').append('<div style="text-align: center;">There is no comments yet on this article. Be the first to share what you think !</div>');
+        }
     });
 });
 
